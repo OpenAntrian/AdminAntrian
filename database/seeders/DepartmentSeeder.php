b3d6f16d09c1c42a5ec44466e74d292f2a76dddd
udamuri <udamuri@gmail.com>
2021-10-09 12:51:12 +0700

    user role

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DepartmentSeeder extends Seeder
 {
@@ -13,6 +15,40 @@ class DepartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $arrayData = [
+            [
+                'name' => 'Super Admin',
+                'email' => 'super@admin.com',
+                'password' => Hash::make("12345678"),
+                'phone' => "083840399512",
+                'level' => 84,
+            ],
+            [
+                'name' => 'Ahmad Al Fatih Budiman',
+                'email' => 'muribudiman@gmail.com',
+                'password' => Hash::make("123456"),
+                'level' => 85,
+            ],
+            [
+                'name' => 'Hawa Inshira Budiman',
+                'email' => 'muri.budiman@amscloud.co.id',
+                'password' => Hash::make("123456"),
+                'level' => 85,
+            ],
+            [
+                'name' => 'Skala',
+                'email' => 'test@skala.dev',
+                'password' => Hash::make("123456")
+            ],
+            [
+                'name' => 'Zohan Nazarudin S.Kom, M.Kom',
+                'email' => 'zohan69@gmail.com',
+                'password' => Hash::make("123456")
+            ],
+        ];
+        
+        foreach($arrayData as $key => $value) {
+            User::create($value);
+        }
     }
 }

9e11e416c3ca2a17d1b84ee29bd3eb970511223c
udamuri <udamuri@gmail.com>
2021-10-22 18:35:50 +0700

    profile picture

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Bypass Policy
+        Gate::before(function (User $user) {
+            if ($user->hasRole('admin') || $user->hasRole('super-admin')) {
+                return true;
+            }
+        });
     }
 }
--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -19,7 +19,7 @@
                 <jet-label for="photo" value="Photo" />
 
                 <!-- Current Profile Photo -->
-                <div class="mt-2" v-show="! photoPreview">
+                <div class="mt-2" v-show="!photoPreview">
                     <img :src="user.profile_photo_url" :alt="user.name" class="rounded-full h-20 w-20 object-cover">
                 </div>
 
@@ -113,7 +113,9 @@
                 this.form.post(route('user-profile-information.update'), {
                     errorBag: 'updateProfileInformation',
                     preserveScroll: true,
-                    onSuccess: () => (this.clearPhotoFileInput()),
+                    onSuccess: () => (
+                        this.clearPhotoFileInput()
+                    ),
                 });
             },
 
